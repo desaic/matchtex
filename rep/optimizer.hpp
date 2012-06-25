7cdb5c48c6e8385082fa79fc2cf0a446ef5c9862
Desai Chen <desaicwtf@gmail.com>
2012-06-25 19:08:34 -0400

    rm comment

--- rep/optimizer.hpp
+++ rep/optimizer.hpp
@@ -1,17 +1,10 @@
-#pragma once
-
-#ifndef __OPTIMIZER__
-#define __OPTIMIZER__
+#ifndef OPTIMIZER_HPP
+#define OPTIMIZER_HPP
 
 #include <vector>
 #include <math.h>
 #include <stdlib.h>
 #include <cstdlib>
-//#include "set.h"
-//#include "searchtree.h"
-
-#include <functional>
-//#include <boost/icl/interval_set.hpp>
 
 #define debug(S, ...) fprintf(stderr, S, __VA_ARGS__);
 
@@ -32,15 +25,9 @@ namespace Fab {
 
   class Optimizer {
 	public:
-//		typedef typename SearchSpace::State              State;
-	//	virtual void setSearchSpace( SearchSpace searchSpace) = 0;
-
     virtual State * getState()=0;
     virtual void setState(State * state)=0;
-		//virtual void run( int step) = 0;
-		//virtual void run( double errorThreshold ) = 0;
     virtual void run()=0;
-		//virtual void runStep() = 0;
 	};
 
 }
